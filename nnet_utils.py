e1d556d9ef6db556163fd880f7709896b6487edb
Rafael Valle <jrafaelvalle@gmail.com>
2016-04-14 21:02:33 -0700

    iterate through batches, etc. might be removed

---
+++ nnet_utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import theano
+
+
+def get_next_batch(inputs, targets, batch_size, n_iters):
+    for _ in range(n_iters):
+        excerpt = np.random.permutation(len(inputs))[:batch_size]
+        yield inputs[excerpt], targets[excerpt]
+
+
+def get_next_batch_rnn(inputs, targets, masks, batch_size, n_iters):
+    for _ in range(n_iters):
+        excerpt = np.random.permutation(len(inputs))[:batch_size]
+        yield inputs[excerpt], targets[excerpt], masks[excerpt]
+
+
+def floatX(X):
+    return np.asarray(X, dtype=theano.config.floatX)
+
+
+def zerosX(X):
+    return np.zeros(X, dtype=theano.config.floatX)
+
+
+def init_weights(shape):
+    return theano.shared(floatX(np.random.randn(*shape) * 0.01))
