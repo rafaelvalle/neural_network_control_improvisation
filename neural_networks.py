b70c0347f77c0d50792a1766d546961cd55abfcc
Rafael Valle <jrafaelvalle@gmail.com>
2017-01-18 02:55:08 -0800

    adding methods for rnn proll

--- neural_networks.py
+++ neural_networks.py
@@ -4,15 +4,11 @@ import theano
 from theano import tensor as T
 import lasagne
 import nnet_utils
-import seaborn
-import pylab as plt
-plt.use('Agg')
+import matplotlib as mpl
+import matplotlib.pylab as plt
+mpl.use('Agg')
 
-
-def set_trace():
-    from IPython.core.debugger import Pdb
-    import sys
-    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+import pdb
 
 
 def train_sequence_rnn(data, layers, updates_fn, batch_size=16, epoch_size=128,
@@ -28,7 +24,6 @@ def train_sequence_rnn(data, layers, updates_fn, batch_size=16, epoch_size=128,
     cost = T.mean((prediction.flatten() - target_var)**2)
     # create parameter update expressions for training
     params = lasagne.layers.get_all_params(layers[2], trainable=True)
-
     print("Computing updates ...")
     updates = updates_fn(cost, params)
 
@@ -94,6 +89,95 @@ def train_sequence_rnn(data, layers, updates_fn, batch_size=16, epoch_size=128,
                 if epoch_result['validate_cost'] < patience_cost:
                     # Increase patience by the supplied about
                     patience += epoch_size*patience_increase
+                # Even if we didn't increase patience, update lowest valid cost
+                current_val_cost = epoch_result['validate_cost']
+            # Store patience after this epoch
+            epoch_result['patience'] = patience
+
+            if n > patience:
+                break
+
+            yield epoch_result
+
+
+def train_rnn_proll(data_iter, layers, updates_fn, batch_size=32,
+                    epoch_size=128, initial_patience=1000,
+                    improvement_threshold=0.99, patience_increase=5,
+                    max_iter=100000):
+    # get input and mask vars from layers and specifiy output var
+    input_var = layers[0].input_var
+    mask_var = layers[1].input_var
+    target_var = T.imatrix('target')
+
+    # create a cost expression for training
+    prediction = lasagne.layers.get_output(layers[2])
+    cost = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+    cost = cost.mean()
+
+    # create parameter update expressions for training, compile func for train
+    params = lasagne.layers.get_all_params(layers[2], trainable=True)
+    updates = updates_fn(cost, params)
+    train_fn = theano.function(inputs=[input_var, target_var, mask_var],
+                               outputs=cost,
+                               updates=updates)
+    train_pred_fn = theano.function([input_var, mask_var], prediction)
+
+    # create cost expression for validation
+    # deterministic forward pass to disable droupout layers
+    val_prediction = lasagne.layers.get_output(layers[2], deterministic=True)
+    val_cost = lasagne.objectives.categorical_crossentropy(
+        val_prediction, target_var)
+    val_cost = val_cost.mean(keepdims=True)
+    val_err_rate = T.mean(
+        T.neq(T.argmax(val_prediction, axis=1), T.argmax(target_var, axis=1)),
+        keepdims=True)
+
+    # compile funcs for validation cost and prediction
+    validate_fn = theano.function(
+        inputs=[input_var, target_var, mask_var],
+        outputs=[val_cost, val_err_rate])
+    # val_pred_fn = theano.function([input_var, mask_var], val_prediction)
+
+    # create data iterators
+    patience = initial_patience
+    current_val_cost = np.inf
+    train_cost = 0.0
+    print("Training and Validating ...")
+    for n, (x_batch, y_batch, mask_batch) in enumerate(data_iter['train']):
+        train_cost_cur = train_fn(x_batch, y_batch, mask_batch)
+        train_cost += train_cost_cur
+        train_pred = train_pred_fn(x_batch, mask_batch)        
+
+        # Stop training if NaN is encountered
+        if not np.isfinite(train_cost):
+            print 'Bad training er {} at iteration {}'.format(train_cost, n)
+            break
+
+        if n and not (n % epoch_size):
+            # compute validation cost and objective
+            val_input, val_tgt, val_mask = data_iter['valid'].next()
+            val_cost, val_err_rate = validate_fn(val_input, val_tgt, val_mask)
+            val_cost = val_cost[0]
+            val_err_rate = val_err_rate[0]
+
+            epoch_result = {'iteration': n,
+                            'train_cost_cur': train_cost_cur,
+                            'train_cost': train_cost / float(n),
+                            'validate_cost': val_cost,
+                            'validate_objective': val_err_rate}
+
+            # compute predictions
+            # predictions = val_pred_fn(val_input, val_mask)
+            # pdb.set_trace()
+
+            # Test whether this validate cost is the new smallest
+            if epoch_result['validate_cost'] < current_val_cost:
+                # To update patience, we must be smaller than
+                # improvement_threshold * (previous lowest validation cost)
+                patience_cost = improvement_threshold * current_val_cost
+                if epoch_result['validate_cost'] < patience_cost:
+                    # Increase patience by the supplied about
+                    patience += epoch_size * patience_increase
                 # Even if we didn't increase patience, update lowest valid cost
                 current_val_cost = epoch_result['validate_cost']
             # Store patience after this epoch
@@ -354,6 +438,32 @@ def build_rnn(input_shape, mask_shape, n_hidden, grad_clip, init,
     # output is one dense layer with one output unit
     l_out = lasagne.layers.DenseLayer(
         l_concat, num_units=1, nonlinearity=non_linearities[2])
+    return l_in, l_mask, l_out
+
+
+def build_rnn_proll(input_shape, mask_shape, n_hidden, grad_clip, init,
+                    non_linearities):
+    # input layer
+    l_in = lasagne.layers.InputLayer(shape=input_shape)
+    # mask determines which indices are part of the sequence for each batch
+    l_mask = lasagne.layers.InputLayer(shape=mask_shape)
+    # bidirectional network
+    l_forward = lasagne.layers.RecurrentLayer(
+        l_in, n_hidden, mask_input=l_mask, grad_clipping=grad_clip,
+        W_in_to_hid=lasagne.init.HeUniform(),
+        W_hid_to_hid=lasagne.init.HeUniform(),
+        nonlinearity=non_linearities[0], only_return_final=True)
+    l_backward = lasagne.layers.RecurrentLayer(
+        l_in, n_hidden, mask_input=l_mask, grad_clipping=grad_clip,
+        W_in_to_hid=lasagne.init.HeUniform(),
+        W_hid_to_hid=lasagne.init.HeUniform(),
+        nonlinearity=non_linearities[1], only_return_final=True,
+        backwards=True)
+    # concatenate output of forward and backward layers
+    l_concat = lasagne.layers.ConcatLayer([l_forward, l_backward])
+    # output is one dense layer with n ouput units
+    l_out = lasagne.layers.DenseLayer(
+        l_concat, num_units=12, nonlinearity=non_linearities[2])
     return l_in, l_mask, l_out
 
 
