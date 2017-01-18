6a3c3920d3641b462323678afd66c72dff9d4260
Rafael Valle <jrafaelvalle@gmail.com>
2017-01-18 02:55:53 -0800

    adding params for rnn_proll

--- params.py
+++ params.py
@@ -1,15 +1,21 @@
 from __future__ import division
 import lasagne
 
-n_timesteps = 12*4
-note_resolution = 128
+batch_size = 128
+val_size = 512
+test_size = 1024
+epoch_size = 2048 / 32
+n_timesteps = 7
+note_resolution = epoch_size
 n_pitches = n_timesteps * .5
 offset = 60
 viol_ratio = 0.5
 n_obs = int(1e+4)
-n_features = 1
-max_len = n_timesteps
-min_len = max_len // 2
+n_features = 12
+int_maj = [0, 2, 2, 1, 2, 2, 2]
+int_min = [0, 2, 1, 2, 2, 1, 2]
+max_len = len(int_maj)
+min_len = 3
 
 # pitch sets
 specs = {1: (((0, 4, 7, 10), 0), ),
@@ -24,7 +30,7 @@ specs = {1: (((0, 4, 7, 10), 0), ),
 # neural network parameter not to be explored with bayesian parameter estimation
 nnet_params = {
     'conv_rnn': {'batch_size': 16,
-                 'epoch_size': 128,
+                 'epoch_size': epoch_size,
                  'input_shape': (16, n_timesteps, n_features),
                  'n_filters':  18,
                  'filter_size': (6, 1),
@@ -38,10 +44,10 @@ nnet_params = {
                                      lasagne.nonlinearities.linear),  # output
                  'update_func': lasagne.updates.adadelta
                  },
-    'rnn': {'batch_size': 16,
-            'epoch_size': 128,
-            'input_shape': (16, n_timesteps, n_features),
-            'mask_shape': (16, n_timesteps),
+    'rnn': {'batch_size': batch_size,
+            'epoch_size': epoch_size,
+            'input_shape': (batch_size, n_timesteps, n_features),
+            'mask_shape': (batch_size, n_timesteps),
             'n_hidden': 1024,
             'grad_clip': 100,
             'init': lasagne.init.HeUniform(),
@@ -50,9 +56,22 @@ nnet_params = {
                                 lasagne.nonlinearities.linear),  # output layer
             'update_func': lasagne.updates.adadelta
             },
+    'rnn_proll': {'batch_size': batch_size,
+                  'epoch_size': epoch_size,
+                  'input_shape': (batch_size, n_timesteps, n_features),
+                  'mask_shape': (batch_size, n_timesteps),
+                  'n_hidden': 128,
+                  'grad_clip': 100,
+                  'init': lasagne.init.HeUniform(),
+                  'non_linearities': (
+                      lasagne.nonlinearities.rectify,  # feedforward
+                      lasagne.nonlinearities.rectify,  # feedbackward
+                      lasagne.nonlinearities.softmax),  # output layer
+                  'update_func': lasagne.updates.adadelta
+                  },
     'proll': {'n_layers': 4,
-              'batch_size': 16,
-              'epoch_size': 128,
+              'batch_size': batch_size,
+              'epoch_size': epoch_size,
               'widths': [None, 1024, 1024, n_timesteps*note_resolution],
               'non_linearities': (None,
                                   lasagne.nonlinearities.rectify,
@@ -61,8 +80,8 @@ nnet_params = {
               'update_func': lasagne.updates.adadelta
               },
     'seq': {'n_layers': 4,
-            'batch_size': 16,
-            'epoch_size': 128,
+            'batch_size': batch_size,
+            'epoch_size': epoch_size,
             'widths': [None, 1024, 1024, n_timesteps],
             'non_linearities': (None,
                                 lasagne.nonlinearities.rectify,
@@ -88,6 +107,12 @@ hyperparameter_space = {
         'momentum': {'type': 'float', 'min': 0., 'max': 1.},
         'learning_rate': {'type': 'float', 'min': .000001, 'max': .1},
         'network': {'type': 'enum', 'options': ['rnn_network']}
+        },
+
+    'rnn_proll': {
+        'momentum': {'type': 'float', 'min': 0., 'max': 1.},
+        'learning_rate': {'type': 'float', 'min': .000001, 'max': .1},
+        'network': {'type': 'enum', 'options': ['rnn_proll_network']}
         },
 
     'conv_rnn': {
