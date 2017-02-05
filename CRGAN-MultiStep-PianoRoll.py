cleaning code, adding correct non linearity for discriminator, adding commented gradient and prediction fns

CRGAN-MultiStep-PianoRoll.py
@@ -19,7 +19,7 @@ datapath = '/Users/rafaelvalle/Desktop/datasets/Piano/'
 glob_folder = '/Users/rafaelvalle/Desktop/datasets/Piano/*/'
 glob_file = '*.npy'
 data_dict = defaultdict(list)
-n_pieces = 20
+n_pieces = 8
 crop = (32, 96)
 
 for folderpath in glob.glob(glob_folder):
@@ -38,21 +38,22 @@ for folderpath in glob.glob(glob_folder):
         cur_data = cur_data * 2 - 1
         data_dict[composer].append(cur_data)
 
-d_batch_size = g_batch_size = len(data_dict) * 8
+d_batch_size = g_batch_size = len(data_dict) * 16
 min_len = 32
 max_len = 64
 n_timesteps = 128  # 100ms per step
 n_features = data_dict[data_dict.keys()[0]][0].shape[1]
 n_conditions = len(data_dict.keys())
-temperature = 1
-n_units_d = 32
+temperature = 1.
+n_units_d = 8
 n_units_g = 32
 
+
 def output_nonlinearity(data, temperature=1):
-    return T.clip(lasagne.nonlinearities.softmax(data / temperature),
+    return T.clip(lasagne.nonlinearities.sigmoid(data / temperature),
                   1e-7, 1 - 1e-7)
 
-softmax_temperature = functools.partial(
+sigmoid_temperature = functools.partial(
     output_nonlinearity, temperature=temperature)
 
 arch = 1
@@ -72,15 +73,15 @@ if arch == 1:
                'n_output_units': 1,
                'n_units': n_units_d,
                'grad_clip': 100.,
-               'init': lasagne.init.HeUniform(),
+               'init': lasagne.init.Orthogonal,
                'non_linearities': (
                    lasagne.nonlinearities.rectify,  # feedforward
                    lasagne.nonlinearities.rectify,  # feedbackward
-                   softmax_temperature),  # apply sotfmax with temperature
-               'learning_rate': 0.001,
-               'regularization': 1e-4,
+                   sigmoid_temperature),  # sigmoid with temperature
+               'learning_rate': 0.01,
+               'regularization': 0.0,
                'unroll': 0,
-               'iterations_pre': 5,
+               'iterations_pre': 0,
                }
 
     # declare generator specs
@@ -92,7 +93,7 @@ if arch == 1:
                'output_shape':  (g_batch_size, n_timesteps, n_features),
                'n_units': [32, 64, n_features],
                'grad_clip': 100.,
-               'init': lasagne.init.HeUniform(),
+               'init': lasagne.init.Orthogonal(),
                'non_linearities': (
                    lasagne.nonlinearities.rectify,  # forw and backward data
                    lasagne.nonlinearities.rectify,  # forw and backward noise
@@ -101,84 +102,9 @@ if arch == 1:
                'learning_rate': 0.01
                }
 elif arch == 2:
-    # declare theano variables
-    d_in_X = T.ftensor3('ddata')
-    d_in_M = T.imatrix('dismask')
-    g_in_D = T.ftensor3('gdata')
-    g_in_Z = T.ftensor3('noise')
-    g_in_C = T.ftensor3('condition')
-    g_in_M = T.imatrix('genmask')
-
-    # declare discriminator specs
-    d_specs = {'input_shape': (None, None, n_features),
-               'mask_shape': (None, None),
-               'n_output_units': n_labels,
-               'n_units': 32,
-               'grad_clip': 10.,
-               'init': lasagne.init.HeUniform(),
-               'non_linearities': (
-                   lasagne.nonlinearities.rectify,  # feedforward
-                   lasagne.nonlinearities.rectify,  # feedbackward
-                   softmax_temperature),  # apply sotfmax with temperature
-               'learning_rate': 0.01
-               }
-
-    # declare generator specs
-    g_specs = {'input_shape': (g_batch_size, max_len, n_features),
-               # 'noise_shape': (g_batch_size, max_len, 1),
-               'noise_shape': (g_batch_size, max_len, n_features),
-               'cond_shape': (g_batch_size, max_len, n_conditions),
-               'mask_shape': (g_batch_size, max_len),
-               'output_shape':  (g_batch_size, n_timesteps, n_features),
-               'n_units': [32, 64, n_features],
-               'grad_clip': 100.,
-               'init': lasagne.init.HeUniform(),
-               'non_linearities': (
-                   lasagne.nonlinearities.rectify,  # forw and backward data
-                   lasagne.nonlinearities.rectify,  # forw and backward noise
-                   lasagne.nonlinearities.rectify,  # data and noise lstm concat
-                   lasagne.nonlinearities.tanh),
-               'learning_rate': 0.01
-               }
+    raise Exception("arch 2 not implemented")
 elif arch == 3:
-    # declare theano variables
-    d_in_X = T.ftensor3('ddata')
-    d_in_M = T.imatrix('dismask')
-    g_in_D = T.ftensor3('gdata')
-    g_in_Z = T.ftensor3('noise')
-    g_in_C = T.ftensor3('condition')
-    g_in_M = T.imatrix('genmask')
-
-    # declare discriminator specs
-    d_specs = {'input_shape': (None, None, n_features),
-               'mask_shape': (None, None),
-               'n_output_units': n_labels,
-               'n_units': n_units_d,
-               'grad_clip': 100.,
-               'init': lasagne.init.HeUniform(),
-               'non_linearities': (
-                   lasagne.nonlinearities.rectify,  # feedforward
-                   lasagne.nonlinearities.rectify,  # feedbackward
-                   softmax_temperature),  # apply sotfmax with temperature
-               'learning_rate': 0.0001,
-               'regularization': 1e-7
-               }
-
-    # declare generator specs
-    g_specs = {'input_shape': (g_batch_size, max_len, n_features),
-               'noise_shape': (g_batch_size, max_len, n_features/2),
-               'cond_shape': (g_batch_size, max_len, n_conditions),
-               'mask_shape': (g_batch_size, max_len),
-               'output_shape':  (g_batch_size, n_timesteps, n_features),
-               'n_units': [64, n_features],
-               'grad_clip': 10.,
-               'init': lasagne.init.HeUniform(),
-               'non_linearities': (
-                   lasagne.nonlinearities.rectify,  # forw and backward data
-                   lasagne.nonlinearities.rectify,  # forw and backward noise
-                   lasagne.nonlinearities.tanh),
-               'learning_rate': 0.0005
-               }
+    raise Exception("arch 2 not implemented")
 
 # lstm parameters
 gate_parameters = lasagne.layers.recurrent.Gate(
@@ -193,6 +119,7 @@ cell_parameters = lasagne.layers.recurrent.Gate(
     W_cell=None, b=lasagne.init.Constant(0.),
     nonlinearity=lasagne.nonlinearities.tanh)
 
+
 def build_discriminator(params):
     # input layers
     l_in = lasagne.layers.InputLayer(
@@ -201,14 +128,14 @@ def build_discriminator(params):
         shape=params['mask_shape'], name='d_mask')
 
     # recurrent layers for bidirectional network
-    l_forward = lasagne.layers.RecurrentLayer(
+    l_forward = lasagne.layers.LSTMLayer(
         l_in, params['n_units'], grad_clipping=params['grad_clip'],
-        W_in_to_hid=params['init'], W_hid_to_hid=params['init'],
+        # W_in_to_hid=params['init'], W_hid_to_hid=params['init'],
         nonlinearity=params['non_linearities'][0], only_return_final=True,
         mask_input=l_mask)
-    l_backward = lasagne.layers.RecurrentLayer(
+    l_backward = lasagne.layers.LSTMLayer(
         l_in, params['n_units'], grad_clipping=params['grad_clip'],
-        W_in_to_hid=params['init'], W_hid_to_hid=params['init'],
+        # W_in_to_hid=params['init'], W_hid_to_hid=params['init'],
         nonlinearity=params['non_linearities'][1], only_return_final=True,
         mask_input=l_mask, backwards=True)
 
@@ -303,70 +230,9 @@ def build_generator_lstm(params, arch=1):
             W=lasagne.init.HeNormal(gain=1.0), b=lasagne.init.Constant(0.1),
             nonlinearity=params['non_linearities'][3])
     elif arch == 2:
-        # input layers
-        l_in = lasagne.layers.InputLayer(
-            shape=params['input_shape'], input_var=g_in_D, name='g_in')
-        l_noise = lasagne.layers.InputLayer(
-            shape=params['noise_shape'], input_var=g_in_Z, name='g_noise')
-        l_cond = lasagne.layers.InputLayer(
-            shape=params['cond_shape'], input_var=g_in_C, name='g_cond')
-        l_mask = lasagne.layers.InputLayer(
-            shape=params['mask_shape'], input_var=g_in_M, name='g_mask')
         raise Exception("arch 2 not implemented")
     elif arch == 3:
-        # input layers
-        l_in = lasagne.layers.InputLayer(
-            shape=params['input_shape'], input_var=g_in_D, name='g_in')
-        l_noise = lasagne.layers.InputLayer(
-            shape=params['noise_shape'], input_var=g_in_Z, name='g_noise')
-        l_cond = lasagne.layers.InputLayer(
-            shape=params['cond_shape'], input_var=g_in_C, name='g_cond')
-        l_mask = lasagne.layers.InputLayer(
-            shape=params['mask_shape'], input_var=g_in_M, name='g_mask')
-
-        # recurrent layers for bidirectional network
-        l_forward_data = lasagne.layers.recurrent.LSTMLayer(
-            l_in, params['n_units'][0], mask_input=l_mask,
-            ingate=gate_parameters, forgetgate=gate_parameters,
-            cell=cell_parameters, outgate=gate_parameters,
-            learn_init=True, grad_clipping=params['grad_clip'],
-            only_return_final=False,
-            nonlinearity=params['non_linearities'][0])
-        l_forward_noise = lasagne.layers.recurrent.LSTMLayer(
-            l_noise, params['n_units'][0], mask_input=l_mask,
-            ingate=gate_parameters, forgetgate=gate_parameters,
-            cell=cell_parameters, outgate=gate_parameters,
-            learn_init=True, grad_clipping=params['grad_clip'],
-            only_return_final=False,
-            nonlinearity=params['non_linearities'][1])
-
-        l_backward_data = lasagne.layers.recurrent.LSTMLayer(
-            l_in, params['n_units'][0], mask_input=l_mask,
-            ingate=gate_parameters, forgetgate=gate_parameters,
-            cell=cell_parameters, outgate=gate_parameters,
-            learn_init=True, grad_clipping=params['grad_clip'],
-            only_return_final=False, backwards=True,
-            nonlinearity=params['non_linearities'][0])
-        l_backward_noise = lasagne.layers.recurrent.LSTMLayer(
-            l_noise, params['n_units'][0], mask_input=l_mask,
-            ingate=gate_parameters, forgetgate=gate_parameters,
-            cell=cell_parameters, outgate=gate_parameters,
-            learn_init=True, grad_clipping=params['grad_clip'],
-            only_return_final=False, backwards=True,
-            nonlinearity=params['non_linearities'][1])
-
-        # concatenate output of forward and backward layers
-        l_lstm_concat = lasagne.layers.ConcatLayer(
-            [l_forward_data, l_forward_noise, l_backward_data,
-             l_backward_noise], axis=2)
-
-        # dense layer with dense layer lstm and condition, w/dropout
-        l_out = lasagne.layers.DenseLayer(
-            lasagne.layers.DropoutLayer(l_lstm_concat, p=0.5),
-            num_units=params['n_units'][1],
-            num_leading_axes=2,
-            W=lasagne.init.HeNormal(gain=1.0), b=lasagne.init.Constant(0.1),
-            nonlinearity=params['non_linearities'][2])
+        raise Exception("arch 2 not implemented")
 
     class Generator:
         def __init__(self, l_in, l_noise, l_cond, l_mask, l_out):
@@ -379,8 +245,7 @@ def build_generator_lstm(params, arch=1):
     return Generator(l_in, l_noise, l_cond, l_mask, l_out)
 
 
-def sample_data(data, batch_size, min_len, max_len, clip=False,
-                p_noise=1.0, noise_mean=0.0, noise_var=0.1):
+def sample_data(data, batch_size, min_len, max_len, clip=False):
     encoding = defaultdict(lambda _: 0)
     i = 0
     for k in data.keys():
@@ -390,35 +255,32 @@ def sample_data(data, batch_size, min_len, max_len, clip=False,
     pieces_per_lbl = int(batch_size / len(data.keys()))
 
     while True:
-        inputs, targets, conds, masks = [], [], [], []
+        inputs, conds, masks = [], [], []
         for k, lbl in encoding.items():
                 pieces = np.random.choice(data[k], pieces_per_lbl)
                 for piece in pieces:
-                    start_idx = np.random.randint(0, piece.shape[0] - max_len - 1)
+                    start_idx = np.random.randint(0, piece.shape[0] -max_len -1)
                     piece_data = piece[start_idx: start_idx+max_len]
                     mask_size = np.random.randint(min_len, max_len)
                     if clip:
                         piece_data[mask_size:] = 0
-                    target = piece[start_idx+max_len]
 
-                    cond = np.zeros((max_len, len(encoding)), dtype=np.float32) #+1 for generator label
+                    cond = np.zeros((max_len, len(encoding)), dtype=np.float32)
                     cond[:, encoding[k]] = 1
 
                     mask = np.zeros(max_len, dtype=np.int32)
                     mask[:mask_size] = 1
 
                     inputs.append(piece_data)
-                    targets.append(target)
                     conds.append(cond)
                     masks.append(mask)
 
-        inputs = np.array(inputs, dtype=np.float32)
-        targets = np.array(targets, dtype=np.float32)
-        conds = np.array(conds, dtype=np.float32)
-        masks = np.array(masks, dtype=np.int32)
-
         shuffle_ids = np.random.randint(0, len(inputs), len(inputs))
-        yield inputs[shuffle_ids], targets[shuffle_ids], conds[shuffle_ids], masks[shuffle_ids]
+        inputs = np.array(inputs, dtype=np.float32)[shuffle_ids]
+        conds = np.array(conds, dtype=np.float32)[shuffle_ids]
+        masks = np.array(masks, dtype=np.int32)[shuffle_ids]
+
+        yield inputs, conds, masks
 
 
 def build_training(discriminator, generator, d_specs, g_specs, orig_loss=False):
@@ -432,23 +294,25 @@ def build_training(discriminator, generator, d_specs, g_specs, orig_loss=False):
                                             generator.l_cond: g_in_C,
                                             generator.l_mask: g_in_M})
 
-    # G(z) mask
-    g_z_M = T.ones((g_z.shape[0], g_z.shape[1]))
-
     # D(G(z))
+    g_z_M = T.ones((g_z.shape[0], g_z.shape[1]))
     d_g_z = lasagne.layers.get_output(discriminator.l_out,
                                       inputs={discriminator.l_in: g_z,
                                               discriminator.l_mask: g_z_M})
-
     # D(x)
     d_x = lasagne.layers.get_output(discriminator.l_out,
                                     inputs={discriminator.l_in: d_in_X,
                                             discriminator.l_mask: d_in_M})
     # loss functions
-    g_loss = -T.log(T.clip(d_g_z, 1e-7, 1.0 - 1e-7))
+    g_loss = lasagne.objectives.binary_crossentropy(
+        T.clip(d_g_z, 1e-7, 1.0 - 1e-7), 0.9)
     g_loss = g_loss.mean()
-    d_x_loss = -T.log(T.clip(d_x , 1e-7, 1.0 - 1e-7))
-    d_g_z_loss = -T.log(T.clip(1 - d_g_z, 1e-7, 1.0 - 1e-7))
+
+    d_x_loss = lasagne.objectives.binary_crossentropy(
+        T.clip(d_x, 1e-7, 1.0 - 1e-7), 0.9)
+    d_g_z_loss = lasagne.objectives.binary_crossentropy(
+        T.clip(d_g_z, 1e-7, 1.0 - 1e-7), 0.1)
+
     d_loss = d_x_loss + d_g_z_loss
     d_loss = d_loss.mean()
     d_loss = d_loss + discriminator.regularization
@@ -473,7 +337,6 @@ def build_training(discriminator, generator, d_specs, g_specs, orig_loss=False):
         inputs=[d_in_X, d_in_M, g_in_D, g_in_Z, g_in_C, g_in_M],
         outputs=[d_loss, d_x_loss, d_g_z_loss, d_x, d_g_z, g_z],
         updates=d_updates)
-
     if d_specs.get('unroll', 0):
         g_train_fn = theano.function(
             inputs=[g_in_D, g_in_Z, g_in_C, g_in_M, d_in_M, d_in_X],
@@ -484,16 +347,35 @@ def build_training(discriminator, generator, d_specs, g_specs, orig_loss=False):
             inputs=[g_in_D, g_in_Z, g_in_C, g_in_M],
             outputs=g_loss,
             updates=g_updates)
+    """
+    # gradient functions
+    grad_tn = theano.grad(
+        d_loss, discriminator.l_out.W)
+    # grad_tn = theano.grad(
+    #     d_loss, lasagne.layers.get_all_params(discriminator.l_out)
+    d_grad_fn = theano.function(
+        inputs=[d_in_X, d_in_M, g_in_D, g_in_Z, g_in_C, g_in_M],
+        outputs=[grad_tn])
+
+    # prediction functions
+    d_pred_fn = theano.function(
+        inputs=[d_in_X, d_in_M],
+        outputs=d_x)
+    """
 
     return d_train_fn, g_train_fn
 
-
+print("build discriminator")
 discriminator = build_discriminator(d_specs)
+
+print("build generator")
 generator = build_generator_lstm(g_specs, arch=arch)
 
+print("build training")
 d_train_fn, g_train_fn = build_training(
     discriminator, generator, d_specs, g_specs)
 
+# D(x)
 data_iter = sample_data(data_dict, d_batch_size, min_len, max_len)
 
 # Pre training
@@ -505,15 +387,15 @@ folderpath = (
     '{}_dlr_{}_glr_{}_bs_{}_temp_{}_'
     'nunits_{}_nt{}_crop_{}_'
     'unroll_{}').format(n_d_iterations_pre, d_specs['learning_rate'],
-                         g_specs['learning_rate'], d_batch_size, temperature,
-                         n_units_g, n_timesteps, crop, d_specs['unroll'])
+                        g_specs['learning_rate'], d_batch_size, temperature,
+                        n_units_g, n_timesteps, crop, d_specs['unroll'])
 
 if not os.path.exists(os.path.join('images', folderpath)):
     os.makedirs(os.path.join('images', folderpath))
 
 for i in range(d_specs.get('iterations_pre', 0)):
     # use same data for discriminator and generator
-    d_X, _, d_C, d_M = data_iter.next()
+    d_X, d_C, d_M = data_iter.next()
     g_X, g_C, g_M = d_X, d_C, d_M
 
     # gaussian(spherical) noise
@@ -523,8 +405,8 @@ for i in range(d_specs.get('iterations_pre', 0)):
         d_X, d_M, g_X, g_Z, g_C, g_M)
     d_losses_pre.append(d_loss)
 
-    if i == (n_d_iterations_pre -1):
-        fig, axes = plt.subplots(4, 2, figsize=(16, 20))
+    if i == (n_d_iterations_pre - 1):
+        fig, axes = plt.subplots(4, 2, figsize=(12, 15))
         axes[0, 0].set_title('D(x)')
         sbn.heatmap(d_x.T, ax=axes[1, 0])
         axes[0, 1].set_title('D(G(z))')
@@ -548,14 +430,14 @@ for i in range(d_specs.get('iterations_pre', 0)):
 d_losses = []
 g_losses = []
 n_iterations = int(1e4)
-n_d_iterations = 1
+n_d_iterations = 5
 n_g_iterations = 1
-epoch = 1
+epoch = 100
 
 for iteration in tqdm(range(n_iterations)):
     for i in range(n_d_iterations):
         # load data
-        d_X, _, d_C, d_M = data_iter.next()
+        d_X, d_C, d_M = data_iter.next()
         g_C, g_M = d_C, d_M
         g_Z = np.random.normal(size=g_specs['noise_shape']).astype('float32')
 
@@ -566,14 +448,13 @@ for iteration in tqdm(range(n_iterations)):
         else:
             g_X = d_X
 
-        # train iteration
         d_loss, d_x_loss, d_g_z_loss, d_x, d_g_z, g_z = d_train_fn(
             d_X, d_M, g_X, g_Z, g_C, g_M)
         d_losses.append(d_loss)
 
     for i in range(n_g_iterations):
         # load data
-        d_X, _, d_C, d_M = data_iter.next()
+        d_X, d_C, d_M = data_iter.next()
         g_X, g_C, g_M = d_X, d_C, d_M
         g_Z = np.random.normal(size=g_specs['noise_shape']).astype('float32')
 
@@ -587,9 +468,9 @@ for iteration in tqdm(range(n_iterations)):
     if iteration % epoch == 0:
         fig, axes = plt.subplots(6, 2, figsize=(32, 32))
         axes[0, 0].set_title('D(x)')
-        sbn.heatmap(d_x.T, ax=axes[1, 0])
+        sbn.heatmap(d_x.T, ax=axes[0, 0])
         axes[0, 1].set_title('D(G(z))')
-        sbn.heatmap(d_g_z.T, ax=axes[1, 1])
+        sbn.heatmap(d_g_z.T, ax=axes[0, 1])
 
         axes[1, 0].set_title('D : 0')
         axes[2, 0].set_title('D : 1')