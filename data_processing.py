5832f8d4b0d1fa1ba003d51eea78618aa861923a
Rafael Valle <jrafaelvalle@gmail.com>
2017-02-09 10:54:32 -0800

    adding patch_size and other things

--- data_processing.py
+++ data_processing.py
@@ -2,10 +2,11 @@ import os
 from collections import defaultdict
 import numpy as np
 import glob2 as glob
+import pdb
 
 
 def load_data(datapath, glob_file_str, n_pieces, crop=None, as_dict=True,
-              scale=True):
+              scale=True, patch_size=False, with_labels=False):
     data = []
     if as_dict:
         data = defaultdict(list)
@@ -26,8 +27,18 @@ def load_data(datapath, glob_file_str, n_pieces, crop=None, as_dict=True,
                 cur_data /= cur_data.max(axis=1)[:, None]
                 cur_data = np.nan_to_num(cur_data)
                 cur_data = cur_data * 2 - 1
+            if patch_size:
+                ids = np.arange(0, len(cur_data) - patch_size, patch_size)
+                cur_data = np.array([
+                    cur_data[ids[i-1]:ids[i]] for i in range(1, len(ids))])
             if as_dict:
                 data[composer].append(cur_data)
+            elif patch_size and not with_labels:
+                data.extend(cur_data)
+            elif with_labels:
+                data.extend([cur_data, composer])
             else:
-                data.append([cur_data, composer])
+                data.append(cur_data)
+    if not as_dict:
+        data = np.array(data)
     return data
