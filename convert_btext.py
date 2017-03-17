3aec5492a5e5574f4859db9ef61ca1a32c2583b2
Rafael Valle <jrafaelvalle@gmail.com>
2017-03-16 19:38:34 -0700

    adding encoder decoder to converter

--- convert_btext.py
+++ convert_btext.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+import cPickle as pkl
 import argparse
 import numpy as np
 import glob2 as glob
 
 
-def convert(globstr, samples):
+def convert(globstr, decoder_path, samples):
+    decoder = pkl.load(open(decoder_path, "rb"))
     for filepath in glob.glob(globstr):
         encoded_texts = np.load(filepath)
         print('{}, {}'.format(filepath, encoded_texts.shape))
@@ -14,7 +16,9 @@ def convert(globstr, samples):
             encoded_text = encoded_texts[i]
             if len(encoded_text.shape) == 3:
                 encoded_text = encoded_text[0]
-            cur_txt = ''.join([chr(x) for x in np.argmax(encoded_text, axis=0)])
+
+            cur_txt = decoder.decode(np.argmax(encoded_text, axis=0))
+            cur_txt = ''.join(cur_txt)
             text += '{}, {}\n'.format(i, cur_txt)
         with open(filepath+'.txt', "w") as text_file:
             text_file.write(text)
@@ -23,11 +27,11 @@ def convert(globstr, samples):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("globstr", type=str,
-                        help="Glob string")
+    parser.add_argument("globstr", type=str, help="Glob string")
+    parser.add_argument("decoder", type=str, help="Decoder path")
     parser.add_argument("-s", "--samples", type=int, default=10,
                         help="Number of samples per file")
 
     args = parser.parse_args()
     print(args)
-    convert(args.globstr, args.samples)
+    convert(args.globstr, args.decoder, args.samples)
